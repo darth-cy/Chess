e02e43d3e4d4081fd76134ae34c697cf32a2cff5
App Academy Student <student@appacademy.io>
2015-06-30 15:30:57 -0700

    Populate and display board

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,58 @@
+require "colorize"
+require 'byebug'
+
 class Board
   attr_accessor :rows
 
   def initialize
-    @rows = 1
+    @rows = Array.new(8) { Array.new(8) { EmptyPiece.new } }
+    # @rows[1][1] = Pawn.new(:B, [1,1], 7, PAWN_STEPS)
+    # @rows[1][2] = Pawn.new(:W, [1,2], 7, PAWN_STEPS)
+    (0..7).each do |idx|
+      @rows[1][idx] = Pawn.new(:B, [1, idx], 7, PAWN_STEPS)
+      @rows[6][idx] = Pawn.new(:W, [1, idx], 7, PAWN_STEPS)
+    end
+
+    player = :B
+    row_idxs = [0, -1]
+    col_idxs = (0..7).to_a
+    row_idxs.each do |row_idx|
+      player = :W if row_idx == -1
+      col_idxs.each do |col|
+        case col
+        when 0, 7
+          @rows[row_idx][col] = Rook.new(player, [row_idx, col], 7, ROOK_DIRECTIONS)
+        when 1, 6
+          @rows[row_idx][col] = Knight.new(player, [row_idx, col], 7, KNIGHT_STEPS)
+        when 2, 5
+          @rows[row_idx][col] = Bishop.new(player, [row_idx, col], 7, BISHOP_DIRECTIONS)
+        when 3
+          @rows[row_idx][col] = Queen.new(player, [row_idx, col], 7, QUEEN_DIRECTIONS)
+        when 4
+          @rows[row_idx][col] = King.new(player, [row_idx, col], 7, KING_STEPS)
+        end
+      end
+    end
+
   end
 
   def self.empty_board
-    b = Board.new
-    b.rows = Array.new(8) { Array.new(8) {nil} }
-    return b
+    nil
   end
 
+  def [](pos)
+    row, col = pos
+    @rows[row][col]
+  end
+
+  def render
+    @rows.each_with_index do |row, r_idx|
+      row.each_with_index do |cell, c_idx|
+        color = (r_idx + c_idx) % 2 == 0 ? :red : :black
+        print cell.render.colorize(:background => color)
+      end
+      puts
+    end
+  end
 
 end
--- lib/piece.rb
+++ lib/piece.rb
@@ -5,18 +5,17 @@ QUEEN_DIRECTIONS = [[1,0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [
 BISHOP_DIRECTIONS = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
 KNIGHT_STEPS = [1 , -1, 2, -2].permutation(2).to_a.reject { |direc| direc.first.abs == direc.last.abs }
 KING_STEPS = [[1,0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
-# PAWN_STEPS
+PAWN_STEPS = []
 
 class Piece
-  attr_reader :pos, :directions
+  attr_reader :pos, :directions, :board, :color
 
   def initialize(color, pos, board = Board.empty_board, directions = [])
-    @color
+    @color = color
     @pos = pos
-    @board
+    @board = board
     @directions = directions
   end
-
 
   def moves
     raise "Must implement in subclass!"
@@ -24,6 +23,42 @@ class Piece
 
   def valid?(pos)
     pos.all? { |el| el.between?(0,7) }
+  end
+
+  def enemy?(pos)
+    @board[pos].is_enemy?(@color)
+  end
+
+  def ally?(pos)
+    @board[pos].is_ally?(@color)
+  end
+
+  def is_enemy?(color)
+    @color != color
+  end
+
+  def is_ally?(color)
+    @color == color
+  end
+
+end
+
+class EmptyPiece < Piece
+
+  def initialize
+
+  end
+
+  def is_enemy?(color)
+    false
+  end
+
+  def is_ally?(color)
+    false
+  end
+
+  def render
+    "   "
   end
 
 end
@@ -38,8 +73,11 @@ class SlidingPiece < Piece
       move = self.pos
       until !valid?(move)
         move = [row += direc.first, col += direc.last]
-        valid_moves << move if valid?(move)
+        break if !valid?(move) || ally?(move)
+        valid_moves << move
+        break if enemy?(move)
       end
+
     end
     valid_moves
   end
@@ -54,7 +92,8 @@ class SteppingPiece < Piece
     self.directions.each do |direc|
       row, col = self.pos
       move = [row += direc.first, col += direc.last]
-      valid_moves << move if valid?(move)
+      next if !valid?(move) || ally?(move)
+      valid_moves << move
     end
 
     valid_moves
@@ -66,26 +105,79 @@ end
 
 class Rook < SlidingPiece
 
+  def render
+    @color == :W ? " \u2656 ".encode('utf-8') : " \u265C ".encode('utf-8')
+  end
+
 end
 
 class Queen < SlidingPiece
+
+  def render
+    @color == :W ? " \u2655 ".encode('utf-8') : " \u265B ".encode('utf-8')
+  end
 
 end
 
 class Bishop < SlidingPiece
 
+  def render
+    @color == :W ? " \u2657 ".encode('utf-8') : " \u265D ".encode('utf-8')
+  end
+
 end
 
 class King < SteppingPiece
+
+  def render
+    @color == :W ? " \u2654 ".encode('utf-8') : " \u265A ".encode('utf-8')
+  end
 
 end
 
 class Knight < SteppingPiece
 
+  def render
+    @color == :W ? " \u2658 ".encode('utf-8') : " \u265E ".encode('utf-8')
+  end
+
 end
 
 class Pawn < SteppingPiece
 
+  def initialize(color, pos, board = Board.empty_board, directions = [])
+    super(color, pos, board = Board.empty_board, directions = [])
+    @moved = false
+  end
+
+  def moves
+    valid_moves = []
+
+    row, col = self.pos
+    attack = [[-1, -1], [-1, 1]]
+    attack.map! { |el| el.first *= -1 } if @color == :B
+
+    attack.each do |pos|
+      move = [row + pos.first, col + pos.last]
+      valid_moves << move if enemy?(move)
+    end
+
+    step = (@color == :B) ? 1 : -1
+    move = [row += step, col]
+    return valid_moves if !valid?(move) || ally?(move)
+    valid_moves << move
+
+    move = [row += step, col]
+    return valid_moves if @moved || !valid?(move) || ally?(move) || enemy?(move)
+    valid_moves << move
+    valid_moves
+
+  end
+
+  def render
+    @color == :W ? " \u2659 ".encode('utf-8') : " \u265F ".encode('utf-8')
+  end
+
 end
 
 
@@ -93,13 +185,21 @@ end
 
 
 
-r = Rook.new(:W, [0,0], nil, ROOK_DIRECTIONS)
-p r.moves
-q = Queen.new(:W, [4,4], nil, QUEEN_DIRECTIONS)
-p q.moves
-b = Bishop.new(:W, [7,4], nil, BISHOP_DIRECTIONS)
-p b.moves
-k = King.new(:W, [7, 4], nil, KING_STEPS)
-p  k.moves
-kn = Knight.new(:W, [4, 4], nil, KNIGHT_STEPS)
-p kn.moves
+
+
+# r = Rook.new(:W, [0,0], Board.new, ROOK_DIRECTIONS)
+# q = Queen.new(:W, [4,4], Board.new , QUEEN_DIRECTIONS)
+# p q.moves.count
+# b = Bishop.new(:W, [7,4], Board.new, BISHOP_DIRECTIONS)
+# p b.moves
+# k = King.new(:W, [2, 2], Board.new, KING_STEPS)
+# k.board.rows[1][1] = Knight.new(:W, [1,1], Board.new, KNIGHT_STEPS)
+# k.board.rows[2][1] = Knight.new(:B, [2,1], Board.new, KNIGHT_STEPS)
+# p  k.moves.count
+# kn = Knight.new(:W, [7, 7],Board.new, KNIGHT_STEPS)
+# pawn = Pawn.new(:W, [4, 4], Board.new, PAWN_STEPS)
+# pawn.board.rows[2][4] = Knight.new(:B, [2,4], Board.new, KNIGHT_STEPS)
+# p pawn.moves
+
+b = Board.new
+b.render
