5e9ad68f011c36b80a2a5a5d87c528877146fae8
App Academy Student <student@appacademy.io>
2015-06-30 13:00:09 -0700

    Initialize Knight and King pieces

--- lib/piece.rb
+++ lib/piece.rb
@@ -3,17 +3,18 @@ require_relative 'board'
 ROOK_DIRECTIONS = [[1,0], [0, 1], [-1, 0], [0, -1]]
 QUEEN_DIRECTIONS = [[1,0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
 BISHOP_DIRECTIONS = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
-# KNIGHT_STEPS
-# KING_STEPS
+KNIGHT_STEPS = [1 , -1, 2, -2].permutation(2).to_a.reject { |direc| direc.first.abs == direc.last.abs }
+KING_STEPS = [[1,0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
 # PAWN_STEPS
 
 class Piece
-  attr_reader :pos
+  attr_reader :pos, :directions
 
-  def initialize(color, pos, board = Board.empty_board)
+  def initialize(color, pos, board = Board.empty_board, directions = [])
     @color
     @pos = pos
     @board
+    @directions = directions
   end
 
 
@@ -28,16 +29,8 @@ class Piece
 end
 
 class SlidingPiece < Piece
-  attr_reader :directions
 
-
-  def initialize(color, pos, board = Board.empty_board, directions = [])
-    super(color, pos, board)
-    @directions = directions
-  end
-
-
-  def slide
+  def moves
     valid_moves = []
 
     self.directions.each do |direc|
@@ -53,6 +46,24 @@ class SlidingPiece < Piece
 
 end
 
+class SteppingPiece < Piece
+
+  def moves
+    valid_moves = []
+
+    self.directions.each do |direc|
+      row, col = self.pos
+      move = [row += direc.first, col += direc.last]
+      valid_moves << move if valid?(move)
+    end
+
+    valid_moves
+
+  end
+
+end
+
+
 class Rook < SlidingPiece
 
 end
@@ -65,14 +76,30 @@ class Bishop < SlidingPiece
 
 end
 
+class King < SteppingPiece
+
+end
+
+class Knight < SteppingPiece
+
+end
+
+class Pawn < SteppingPiece
+
+end
+
 
 
 
 
 
 r = Rook.new(:W, [0,0], nil, ROOK_DIRECTIONS)
-p r.slide
+p r.moves
 q = Queen.new(:W, [4,4], nil, QUEEN_DIRECTIONS)
-p q.slide
+p q.moves
 b = Bishop.new(:W, [7,4], nil, BISHOP_DIRECTIONS)
-p b.slide
+p b.moves
+k = King.new(:W, [7, 4], nil, KING_STEPS)
+p  k.moves
+kn = Knight.new(:W, [4, 4], nil, KNIGHT_STEPS)
+p kn.moves
